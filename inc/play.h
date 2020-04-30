b822414ba74d1e61c3c0ca0733548e26d3f12fc3
Mahyar Koshkouei <mk@deltabeard.com>
2020-04-30 23:06:16 +0100

    license: update license headers
    Signed-off-by: Mahyar Koshkouei <mk@deltabeard.com>

--- inc/play.h
+++ inc/play.h
@@ -3,7 +3,7 @@
  * Copyright (C) 2020  Mahyar Koshkouei
  *
  * This is free software, and you are welcome to redistribute it under the terms
- * of the GNU General Public License version 3.
+ * of the GNU Affero General Public License version 3.
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
--- inc/timer.h
+++ inc/timer.h
@@ -1,3 +1,17 @@
+/**
+ * Handles framerate timing.
+ * Copyright (C) 2020  Mahyar Koshkouei
+ *
+ * This is free software, and you are welcome to redistribute it under the terms
+ * of the GNU Affero General Public License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.
+ *
+ * See the LICENSE file for more details.
+ */
+
 #pragma once
 
 #include <SDL2/SDL.h>
