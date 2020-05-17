aa3ac3df2a68cd6b5c541def0ea9ca2614a0a2e0
Mahyar Koshkouei <mk@deltabeard.com>
2020-05-17 15:00:07 +0100

    working
    Signed-off-by: Mahyar Koshkouei <mk@deltabeard.com>

--- src/cap.c
+++ src/cap.c
@@ -27,7 +27,8 @@ struct enc_vid_s
 
 static void x264_log(void *priv, int i_level, const char *fmt, va_list ap)
 {
-	const SDL_LogPriority lvlmap[] = {
+	const SDL_LogPriority lvlmap[] =
+	{
 		SDL_LOG_PRIORITY_ERROR, SDL_LOG_PRIORITY_WARN,
 		SDL_LOG_PRIORITY_INFO, SDL_LOG_PRIORITY_DEBUG
 	};
@@ -49,27 +50,28 @@ enc_vid *vid_enc_init(const char *fileout, int width, int height, double fps)
 	if(ctx == NULL)
 		goto out;
 
+	x264_param_default(&ctx->param);
+
 	/* Get default params for preset/tuning.
 	 * Setting preset to veryfast in order to reduce strain during gameplay. */
 	if(x264_param_default_preset(&ctx->param, "superfast", "zerolatency") < 0)
 		goto err;
 
-	/* Configure non-default params */
-	ctx->param.i_bitdepth = 8;
-	ctx->param.i_csp = X264_CSP_RGB;
-	ctx->param.vui.i_sar_width = ctx->param.i_width  = width;
-	ctx->param.vui.i_sar_height = ctx->param.i_height = height;
-	ctx->param.b_vfr_input = 0;
-	ctx->param.b_repeat_headers = 1;
-	ctx->param.b_annexb = 1;
-	ctx->param.i_threads = 1;
-	ctx->param.b_sliced_threads = 1;
-	ctx->param.i_lookahead_threads = X264_THREADS_AUTO;
 	ctx->param.pf_log = x264_log;
+	ctx->param.i_csp = X264_CSP_RGB;
+	ctx->param.i_bitdepth = 8;
+	ctx->param.b_vfr_input = 0;
 
 	ctx->param.rc.i_rc_method = X264_RC_CRF;
 	ctx->param.rc.f_rf_constant = 14;
 	ctx->param.rc.f_rf_constant_max = 35;
+
+	/* Apply profile restrictions. */
+	if(x264_param_apply_profile(&ctx->param, "high444") < 0)
+		goto err;
+
+	ctx->param.vui.i_sar_width = ctx->param.i_width  = width;
+	ctx->param.vui.i_sar_height = ctx->param.i_height = height;
 
 	SDL_assert(fps < 256.0);
 	ctx->param.i_fps_num = (uint32_t)(fps * 16777216.0);
@@ -77,35 +79,26 @@ enc_vid *vid_enc_init(const char *fileout, int width, int height, double fps)
 	ctx->param.i_timebase_den = ctx->param.i_fps_num;
 	ctx->param.i_timebase_num = ctx->param.i_fps_den;
 
-	ctx->param.vui.b_fullrange = 1;
-
-	/* Apply profile restrictions. */
-	if(x264_param_apply_profile(&ctx->param, "high444") < 0)
-		goto err;
+	ctx->param.i_threads = 1;
+	ctx->param.b_repeat_headers = 0;
 
 	ctx->h = x264_encoder_open(&ctx->param);
 	if(ctx->h == NULL)
 		goto err;
 
-	x264_encoder_parameters( ctx->h, &ctx->param );
-
 	ctx->f = SDL_RWFromFile(fileout, "wb");
 	if(ctx->f == NULL)
 		goto err;
 
-#if 1
-	x264_nal_t *headers;
-	int i_nal;
-	int bytes;
-	if((bytes = x264_encoder_headers(ctx->h, &headers, &i_nal)) < 1)
-	{
-		SDL_SetError("Enable to obtain encoder headers");
-		goto err;
-	}
 
-	if(SDL_RWwrite(ctx->f, headers, bytes, 1) == 0)
+	x264_nal_t *nal;
+	int nnal, s;
+	s = x264_encoder_headers(ctx->h, &nal, &nnal);
+	if(s < 0)
 		goto err;
-#endif
+
+	for(int i = 0; i < nnal; i++)
+		SDL_RWwrite(ctx->f, nal[i].p_payload, nal[i].i_payload, 1);
 
 	ctx->frame = 0;
 out:
@@ -127,19 +120,19 @@ int vid_enc_frame(enc_vid *ctx, SDL_Surface *surf)
 	x264_nal_t *nal;
 
 	SDL_assert(surf->format->format == SDL_PIXELFORMAT_RGB24);
-
-	if(x264_picture_alloc(&pic, ctx->param.i_csp, ctx->param.i_width,
-			ctx->param.i_height) < 0)
-		goto err;
+	x264_picture_init(&pic);
 
 	pic.img.i_csp = X264_CSP_RGB;
-	pic.i_type = X264_TYPE_AUTO;
-	pic.i_qpplus1 = X264_QP_AUTO;
 	pic.img.i_plane = 1;
 	pic.img.i_stride[0] = surf->pitch;
 	pic.img.plane[0] = surf->pixels;
+
+	pic.i_pts = ctx->frame;
+	ctx->frame++;
+
+	pic.i_type = X264_TYPE_AUTO;
 	i_frame_size = x264_encoder_encode(ctx->h, &nal, &i_nal, &pic,
-					   &pic_out);
+	                                   &pic_out);
 	if(i_frame_size < 0)
 	{
 		SDL_SetError("x264: unable to encode frame");
@@ -153,8 +146,8 @@ int vid_enc_frame(enc_vid *ctx, SDL_Surface *surf)
 	if(i_frame_size == 0)
 		goto ret;
 
-	if(SDL_RWwrite(ctx->f, &nal->p_payload, i_frame_size, 1) == 0)
-		goto err;
+	for(int i = 0; i < i_nal; i++)
+		SDL_RWwrite(ctx->f, nal[i].p_payload, nal[i].i_payload, 1);
 
 ret:
 	ret = 0;
@@ -170,17 +163,15 @@ void vid_enc_end(enc_vid *ctx)
 		int i_nal;
 		x264_nal_t *nal;
 		x264_picture_t pic_out;
-		int i_frame_size = x264_encoder_encode(ctx->h, &nal,
-						       &i_nal, NULL,
-							&pic_out);
+		int i_frame_size = x264_encoder_encode(ctx->h, &nal, &i_nal, NULL, &pic_out);
 		if(i_frame_size < 0)
 			goto out;
 
 		if(i_frame_size == 0)
 			continue;
 
-		if(SDL_RWwrite(ctx->f, &nal->p_payload, i_frame_size, 1) == 0)
-			goto out;
+		for(int i = 0; i < i_nal; i++)
+			SDL_RWwrite(ctx->f, nal[i].p_payload, nal[i].i_payload, 1);
 	}
 
 	x264_encoder_close(ctx->h);
@@ -191,3 +182,4 @@ void vid_enc_end(enc_vid *ctx)
 out:
 	return;
 }
+
